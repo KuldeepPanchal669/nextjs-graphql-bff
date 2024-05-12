682ad72479f2779265bb9c1c287097422711b672
kupanchal <kupanchal@expediagroup.com>
2024-05-12 12:34:27 +0530

    Minor Change

--- frontend/src/app/product-list/page.tsx
+++ frontend/src/app/product-list/page.tsx
@@ -12,11 +12,6 @@ const ProductList = () => {
           <OfferListWrapper />
         </Col>
       </Row>
-      {/* <Row>
-        <Col>1 of 3</Col>
-        <Col>2 of 3</Col>
-        <Col>3 of 3</Col>
-      </Row> */}
     </Container>
   );
 };
